70d2f66914a7a4bdff0c3a45d57a75792416203b
Hexiang <hexiang.22@ichat.sp.edu.sg>
2023-05-12 19:28:46 +0800

    tested employees by age

--- test_employee_info.py
+++ test_employee_info.py
@@ -9,6 +9,12 @@ def test_calculate_average_salary():
 
 def test_get_employees_by_dept():
     result = info.get_employees_by_dept("Sales")
-    assert(result == [{"name": "John", "age": 30, "department": "Sales", "salary": 50000}, {"name": "Peter", "age": 40, "department": "Sales", "salary": 60000}])
+    expected = [{"name": "John", "age": 30, "department": "Sales", "salary": 50000}, {"name": "Peter", "age": 40, "department": "Sales", "salary": 60000}]
+    assert(result == expected)
+
+def test_get_employees_by_age_range():
+    result = info.get_employees_by_age_range(20,30)
+    expected = [{"name": "Jane", "age": 25, "department": "Marketing", "salary": 60000}, {"name": "Mary", "age": 23, "department": "Marketing", "salary": 56000}]
+    assert(result == expected)
 
 
